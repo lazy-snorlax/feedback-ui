6803815ec18e122911836b4378f4628e1dcee0be
Anthony Morelli <blacknebula236@gmail.com>
2022-10-27 00:28:51 +1030

    json-server backend + loading icon

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,36 +1,36 @@
-import {motion, AnimatePresence} from 'framer-motion'
-import { useContext } from 'react';
-import PropTypes from 'prop-types'
-import FeedbackItem from './FeedbackItem';
-import FeedbackContext from '../context/FeedbackContext'
+import { motion, AnimatePresence } from "framer-motion";
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./shared/Spinner";
+import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext);
 
-    console.log(feedback);
-    if (!feedback || feedback.length === 0){
-        return <p>No feedback yet</p>
-    }
+  console.log(feedback);
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No feedback yet</p>;
+  }
 
-    return (
-        <div className="feedback-list">
-            <AnimatePresence>
-                {feedback.map((item) => (
-                    <motion.div 
-                        key={item.id}
-                        initial={{opacity: 0}}
-                        animate={{opacity: 1}}
-                        exit={{opacity:0}}
-                    >
-                        <FeedbackItem 
-                            key={item.id} 
-                            item={item} 
-                             />
-                    </motion.div>
-                ))}
-            </AnimatePresence>
-        </div>
-    )
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className="feedback-list">
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default FeedbackList
+export default FeedbackList;
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,21 +1,28 @@
-import { useContext } from 'react';
-import FeedbackContext from '../context/FeedbackContext'
+import { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackStats() {
-    const {feedback} = useContext(FeedbackContext)
-    
-    let avg = feedback.reduce((acc, cur) => {
-        return acc + cur.rating
+  const { feedback } = useContext(FeedbackContext);
+
+  let avg =
+    feedback.reduce((acc, cur) => {
+      console.log(
+        ">>>> Calc Avg",
+        acc,
+        parseInt(cur.rating),
+        parseInt(acc) + parseInt(cur.rating)
+      );
+      return parseInt(acc) + parseInt(cur.rating);
     }, 0) / feedback.length;
 
-    avg = avg.toFixed(1).replace(/[.,]0$/, '')
+  avg = avg.toFixed(1).replace(/[.,]0$/, "");
 
-    return (
-        <div className="feedback-stats">
-            <h4>{feedback.length} Reviews</h4>
-            <h4>Average Rating: {isNaN(avg) ? 0 : avg}</h4>
-        </div>
-    )
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(avg) ? 0 : avg}</h4>
+    </div>
+  );
 }
 
-export default FeedbackStats
+export default FeedbackStats;
